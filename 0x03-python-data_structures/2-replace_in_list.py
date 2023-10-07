1e1a727ea7863a5d346fd3b95b8e6f9e23bff7c1
Yareda21 <ykebede39@yahoo.com>
2023-10-07 00:14:09 -0700

    a

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,6 +4,9 @@
 
 def replace_in_list(my_list, idx, element):
     # Replace an element of a list at a specific position."""
-    if idx < len(my_list) or idx >= 0:
-        my_list[idx] = element
-    return (my_list)
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
