e8fab5ad7f82f10cc926c1332dc2886f910629a2
Yareda21 <ykebede39@yahoo.com>
2023-10-24 06:22:28 -0700

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        """ new Square.
+
+        Args:
+            size (int): size 
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+ this is a readme for the project called 0x06-python-classes
