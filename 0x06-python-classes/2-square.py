1d5a22775eeddee9c5566c8a5b0dd0374b060d89
Yareda21 <ykebede39@yahoo.com>
2023-10-24 06:36:08 -0700

    a

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Definition of class Square."""
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size=0):
+        """Constructs a new Square.
+
+        Parameters:
+            size (int): The dimension of the new square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Definition of class Square."""
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size=0):
+        """Constructs a new Square.
+
+        Parameters:
+            size (int): The dimension of the new square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates and returns the current area of the square."""
+        return self.__size ** 2
