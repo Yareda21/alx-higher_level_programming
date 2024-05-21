54524793601db9f2a0fcbd9cfad8f3d62e1e1456
Yareda21 <ykebede39@yahoo.com>
2024-05-21 07:24:27 -0700

    a

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,45 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+//  the 'request' module to perform an HTTP GET request to the Star Wars API URL.
+request(apiUrl, function (error, response, body) {
+  // Check if there was no error during the HTTP request
+  if (!error && response.statusCode === 200) {
+    // Parse the JSON response bod
+    const movieData = JSON.parse(body);
+    // create an array of promises that fetch the data for each individual character.
+    const characterPromises = movieData.characters.map((characterUrl) => {
+      return new Promise((resolve, reject) => {
+        // Use another 'request' to fetch the data for the individual character.
+        request(characterUrl, function (charError, charResponse, charBody) {
+          // Check if there was no error during the HTTP request
+          if (!charError && charResponse.statusCode === 200) {
+            // Parse the JSON response body
+            const characterData = JSON.parse(charBody);
+            // Resolve the promise with the name of the character.
+            resolve(characterData.name);
+          } else {
+            // reject the promise with the error message if  there was an error during the HTTP request
+            reject(new Error(`Error fetching character data: ${charError}`));
+          }
+        });
       });
-    }
+    });
+
+    Promise.all(characterPromises)
+      .then((characterNames) => {
+        console.log(characterNames.join('\n'));
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  } else {
+    console.error('Error fetching movie data:', error);
   }
 });
+
